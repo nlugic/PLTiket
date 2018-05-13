046833013a841366a6772ce54058943b8b342b1c
Malinovic <malinovicnemanja@yahoo.com>
2018-05-13 19:12:09 +0200

    Stadioni

---
+++ stadiums.sql
@@ -0,0 +1,29 @@
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Old Trafford', 'Manchester', '75653');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Emirates Stadium', 'London', '60260');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'London Stadium', 'London', '60000');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Etihad', 'Manchester', '55097');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Anfield', 'Liverpool', '54079');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'St James Park', 'Newcastle upon Tyne', '52338');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Stamford Bridge', 'London', '41798');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Goodison Park', 'Liverpool', '39571');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'St Marys Stadium', 'Southampton', '32505');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'King Power Stadium', 'Leicester', '32312');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'The Hawthorns', 'West Bromwich', '26850');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'bet365 Stadium', 'Stoke-On-Trent', '30089');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Selhurts park', 'London', '26074');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Kirklees Stadium', 'Huddersfiled', '24500');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Turf Moor', 'Burnley', '21401');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Liberty Stadium', 'Swansea', '20909');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Webley Stadium', 'London', '90000');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Dean Court', 'Bournemouth', '11360');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Vicarage Road', 'Watford', '21577');
+INSERT INTO `Stadium` (`Stadium_Id`, `Name`, `Place`, `Capacity`) VALUES (NULL, 'Falmer Stadium', 'Falmer', '30750');
+
+
+
+
+
+
+
+
+
